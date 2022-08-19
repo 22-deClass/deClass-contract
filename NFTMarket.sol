47d6df0918179e2f129aa574d0e419ac4151de3f
sunkitop <65586851+sunkitop@users.noreply.github.com>
2022-08-19 14:38:06 +0900

    Create NFTMarket.sol

---
+++ NFTMarket.sol
@@ -0,0 +1,113 @@
+pragma solidity >=0.4.24 <=0.5.6;
+
+contract NFTSimple {
+    
+    string public name = "KlayLion";
+    
+    string public symbol = "KL";
+    mapping (uint256 => address) public tokenOwner; 
+    mapping (uint256 => string) public tokenURIs;
+    mapping(address => uint256[]) private _ownedTokens;
+    //onKIP17Received bytes value
+    bytes4 private constant _KIP17_RECEIVED = 0x6745782b;
+
+    function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public returns (bool) {
+        tokenOwner[tokenId]=to;
+        tokenURIs[tokenId]=tokenURI;
+
+        _ownedTokens[to].push(tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public {
+        require(from == msg.sender, "from != msg.sender");
+        require(from == tokenOwner[tokenId], "you are not the owner of the token");
+        
+        _removeTokenFromList(from, tokenId);
+        _ownedTokens[to].push(tokenId);
+
+        tokenOwner[tokenId] = to;
+
+        // if the receiving SmartContract has codes to run, run the code
+        require(
+            _checkOnKIP17Received(from,to,tokenId,_data), "KIP17: transfer to non KIP17Receiver implementer"
+        );
+        
+    }
+
+    function _checkOnKIP17Received(address from, address to, uint256 tokenId, bytes memory _data) internal returns (bool) {
+        bool success;
+        bytes memory returndata;
+
+        if (!isContract(to)) {
+            return true;
+        }
+
+        (success, returndata) = to.call(
+            abi.encodeWithSelector(
+                _KIP17_RECEIVED,
+                msg.sender,
+                from,
+                tokenId,
+                _data
+            )
+        );
+
+        if (
+            returndata.length != 0 &&
+            abi.decode(returndata, (bytes4)) == _KIP17_RECEIVED
+        ) {
+                return true;
+        }
+        return false;
+
+     }
+
+     function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly {size:=extcodesize(account)}
+        return size>0;
+     }
+
+    function _removeTokenFromList(address from, uint256 tokenId) private {
+        uint256 lastTokenIndex=_ownedTokens[from].length- 1;
+        for(uint256 i=0;i<_ownedTokens[from].length;i++){
+                if (tokenId==_ownedTokens[from][i]) {
+                    
+                    _ownedTokens[from][i]=_ownedTokens[from][lastTokenIndex];
+                    _ownedTokens[from][lastTokenIndex]=tokenId;
+                    break;
+                }
+                
+        }
+        _ownedTokens[from].length--;
+    }
+
+    function ownedTokens(address owner) public view returns (uint256[] memory) {
+        return _ownedTokens[owner];
+    }
+    function setTokenUri(uint256 id, string memory uri) public {
+        tokenURIs[id]=uri;
+    }
+}
+
+contract NFTMarket {
+    mapping (uint256 => address) public seller;
+
+    function buyNFT(uint256 tokenId, address NFTAddress) public payable returns (bool) {
+        address payable receiver = address(uint160(seller[tokenId]));
+
+        // 10**18 PEB = 1 Klay
+        receiver.transfer(10**16);
+
+        NFTSimple(NFTAddress).safeTransferFrom(address(this), msg.sender, tokenId, "0x00");
+        return true;
+    }
+
+
+    function onKIP17Received(address operator, address from, uint256 tokenId, bytes memory data) public returns (bytes4) {
+        seller [tokenId] = from;
+
+        return bytes4(keccak256("onKIP17Received(address,address,uint256,bytes)"));
+    }
+
+}
